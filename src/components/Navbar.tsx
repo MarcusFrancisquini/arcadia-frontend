9c0359c59aa0f21b209e994b630c12be31c42764
MarcusFrancisquini <francisquinimarcus@gmail.com>
2025-04-19 00:46:06 -0300

    FEAT: navbar feita

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { BsBookmarkFill } from "react-icons/bs";
+import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
@@ -6,6 +8,14 @@ const Navbar = () => {
       <nav className="navbar">
         <Link to="/" className="navbar-logo">
           arcadia
+        </Link>
+        <div className="search-input-container">
+          <input type="text" placeholder="Buscar jogos..." />
+          <FaSearch className="search-input-icon" />
+        </div>
+        <Link to="/favorites" className="favorite-games">
+          <span>Favoritos</span>
+          <BsBookmarkFill />
         </Link>
       </nav>
     </>

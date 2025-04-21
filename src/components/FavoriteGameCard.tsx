4bb10a4b1f16b2f50568b5aeff81d10a258a223d
MarcusFrancisquini <francisquinimarcus@gmail.com>
2025-04-21 20:49:24 -0300

    FEAT: tela de jogos favoritos

---
+++ src/components/FavoriteGameCard.tsx
@@ -0,0 +1,19 @@
+import { Game } from "../services/api";
+import { Link } from "react-router-dom";
+
+interface GameCardProps {
+  game: Game;
+}
+
+const FavoriteGameCard = ({ game }: GameCardProps) => {
+  return (
+    <Link to="">
+      <figure className="favorite-game-card">
+        <img src={game.imageUrl} alt="game-cover" />
+        <figcaption>{game.name}</figcaption>
+      </figure>
+    </Link>
+  );
+};
+
+export default FavoriteGameCard;
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,8 +1,38 @@
+import { useEffect, useState } from "react";
+import { getGames, Game } from "../services/api";
+
+import FavoriteGameCard from "../components/FavoriteGameCard";
+
 const Favorites = () => {
+  // Get favorite games
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    async function fetchGames() {
+      const gamesList = await getGames();
+      setGames(gamesList);
+    }
+    fetchGames();
+  }, []);
+
+  const favoriteGames = games.filter((game) => game.isFavorite);
+
+  // Favorite games counter
+  const favoriteGamesCounter = favoriteGames.length;
+
   return (
-    <>
-      <h1>Jogos Favoritos</h1>
-    </>
+    <div className="favorite-page-container">
+      <h2>
+        <div className="title-text">
+          my favorite games<span>( {favoriteGamesCounter} )</span>
+        </div>
+      </h2>
+      <div className="favorite-games-container">
+        {favoriteGames.map((game) => (
+          <FavoriteGameCard key={game._id} game={game} />
+        ))}
+      </div>
+    </div>
   );
 };
 

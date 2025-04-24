6c6a08ae8c6bfe71553cc8089996e0d973a6d5e6
MarcusFrancisquini <francisquinimarcus@gmail.com>
2025-04-24 08:57:13 -0300

    FEAT: props do body e começo de detalhes do game

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import { BrowserRouter } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
+
+const AppWrapper = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname.startsWith("/games/")) {
+      document.body.classList.add("blackout-body");
+    } else {
+      document.body.classList.remove("blackout-body");
+    }
+  }, [location]);
+
+  return (
+    <>
+      <Navbar />
+      <AppRoutes />
+    </>
+  );
+};
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
-        <AppRoutes />
+        <AppWrapper />
       </BrowserRouter>
     </>
   );
--- src/pages/GameDetail.tsx
+++ src/pages/GameDetail.tsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getGameById, Game } from "../services/api";
+import { JSX } from "react";
+import { FaWindows, FaPlaystation, FaXbox } from "react-icons/fa";
 
 const GameDetail = () => {
   const { id } = useParams<{ id: string }>();
@@ -15,14 +17,50 @@ const GameDetail = () => {
     fetchGame();
   }, [id]);
 
+  // Normalize platform names
+  const platformType = (platform: string) => {
+    if (platform.toLowerCase().includes("playstation")) return "PlayStation";
+    if (platform.toLowerCase().includes("xbox")) return "Xbox";
+    if (platform.toLowerCase().includes("pc")) return "PC";
+    return platform;
+  };
+
+  // Platform icons
+  const platformIcons: Record<string, JSX.Element> = {
+    PC: <FaWindows size={18} />,
+    Xbox: <FaXbox size={18} />,
+    PlayStation: <FaPlaystation size={18} />,
+  };
+
+  // Unique platforms (no duplicates)
+  const uniquePlatforms = Array.from(
+    new Set(game?.platforms.map(platformType))
+  );
+
   return (
     <>
-      <div className="blackout"></div>
       <div className="game-detail-container">
         <div
           className="game-detail-image"
           style={{ backgroundImage: `url(${game?.imageUrl})` }}
         ></div>
+        <div className="game-dt-left-container">
+          <h1 className="game-dt-title">{game?.name}</h1>
+          <div className="game-dt-desc-container">
+            <span className="game-dt-desc-span">Sobre</span>
+            <p className="game-dt-desc">{game?.description}</p>
+          </div>
+          <div className="game-dt-platform-container">
+            <span className="game-dt-subtitle">Plataformas</span>
+            <div className="game-dt-platforms-container">
+              {uniquePlatforms.map((platform, index) => (
+                <span className="game-dt-platforms" key={index}>
+                  {platformIcons[platform]}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );

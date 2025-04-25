9b9dd5302dfb5f55dfba1bb8ace7ab235a8fb909
MarcusFrancisquini <francisquinimarcus@gmail.com>
2025-04-24 22:58:12 -0300

    FEAT: detalhamento do game feito

--- README.md
+++ README.md
@@ -8,18 +8,3 @@
 ### Para instalar
 
 - `npm i react-icons react-router-dom sass axios`
-
-# USAR DEPOIS
-
-```tsx
-// Release date formatting
-const releaseDate = new Date(game.releaseDate);
-const month = releaseDate
-  .toLocaleString("pt-BR", { month: "short" })
-  .replace(".", "");
-const formattedMonth = month.charAt(0).toUpperCase() + month.slice(1);
-const formattedDate = `${releaseDate
-  .getDate()
-  .toString()
-  .padStart(2, "0")} ${formattedMonth} ${releaseDate.getFullYear()}`;
-```
--- src/pages/GameDetail.tsx
+++ src/pages/GameDetail.tsx
@@ -37,6 +37,28 @@ const GameDetail = () => {
     new Set(game?.platforms.map(platformType))
   );
 
+  // Release date formatting
+  let formattedDate = "";
+
+  if (game?.releaseDate) {
+    const releaseDate = new Date(game.releaseDate);
+    const month = releaseDate
+      .toLocaleString("pt-BR", { month: "short" })
+      .replace(".", "");
+    const formattedMonth = month.charAt(0).toUpperCase() + month.slice(1);
+    formattedDate = `${releaseDate
+      .getDate()
+      .toString()
+      .padStart(2, "0")} ${formattedMonth} ${releaseDate.getFullYear()}`;
+  }
+
+  // Rating formatting
+  const getRating = (rating: number) => {
+    if (rating <= 2) return "rating-bad";
+    if (rating <= 3.5) return "rating-ok";
+    return "rating-good";
+  };
+
   return (
     <>
       <div className="game-detail-container">
@@ -44,20 +66,56 @@ const GameDetail = () => {
           className="game-detail-image"
           style={{ backgroundImage: `url(${game?.imageUrl})` }}
         ></div>
-        <div className="game-dt-left-container">
+        <div className="game-dt-content">
           <h1 className="game-dt-title">{game?.name}</h1>
-          <div className="game-dt-desc-container">
-            <span className="game-dt-desc-span">Sobre</span>
-            <p className="game-dt-desc">{game?.description}</p>
-          </div>
-          <div className="game-dt-platform-container">
-            <span className="game-dt-subtitle">Plataformas</span>
-            <div className="game-dt-platforms-container">
-              {uniquePlatforms.map((platform, index) => (
-                <span className="game-dt-platforms" key={index}>
-                  {platformIcons[platform]}
-                </span>
-              ))}
+          <div className="game-dt-information">
+            <div className="game-dt-info-content">
+              <div className="game-dt-desc-container">
+                <span className="game-dt-desc-span">Sobre</span>
+                <p className="game-dt-desc">{game?.description}</p>
+              </div>
+              <div className="game-dt-information-container">
+                <span className="game-dt-subtitle">Plataformas</span>
+                <div className="game-dt-platforms-container">
+                  {uniquePlatforms.map((platform, index) => (
+                    <span className="game-dt-platforms" key={index}>
+                      {platformIcons[platform]}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="game-dt-information-container">
+                <span className="game-dt-subtitle">Desenvolvedora</span>
+                <p className="game-dt-information-text">{game?.developer}</p>
+              </div>
+              <div className="game-dt-information-container">
+                <span className="game-dt-subtitle">Publicadora</span>
+                <p className="game-dt-information-text">{game?.publisher}</p>
+              </div>
+            </div>
+            <div className="game-dt-info-content">
+              <div className="game-dt-information-container">
+                <span className="game-dt-subtitle">Data de Lançamento</span>
+                <p className="game-dt-information-text">{formattedDate}</p>
+              </div>
+              <div className="game-dt-information-container">
+                <span className="game-dt-subtitle">Nota</span>
+                {game?.rating !== undefined && (
+                  <div className={`game-rating ${getRating(game.rating)}`}>
+                    {game?.rating.toFixed(1)}
+                  </div>
+                )}
+              </div>
+              <div className="game-dt-information-container">
+                <span className="game-dt-subtitle">Gênero</span>
+                <p className="game-dt-information-text">
+                  {game?.genres?.join(", ")}
+                </p>
+              </div>
+              <div className="game-dt-information-container">
+                <span className="game-dt-subtitle">Classificação</span>
+                <p className="game-dt-information-text">{game?.ageRating}</p>
+              </div>
             </div>
           </div>
         </div>
